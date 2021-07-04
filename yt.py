8dc53a75209d7ab997d09201d2cb9bf1c0773af9
Bartlomiej Mikler <bartlomiej.mikler@gmail.com>
2021-07-04 23:29:29 +0200

    comverter to mp3 added

--- yt.py
+++ yt.py
@@ -1,4 +1,6 @@
 from pytube import YouTube
+import ffmpy
+import os
 
 yt = input("Videos URL: ")
 
@@ -7,3 +9,14 @@ yt = YouTube(yt)
 stream = yt.streams.first()
 
 stream.download()
+
+ff = ffmpy.FFmpeg(
+    inputs = {yt.title + ".mp4" : None}, 
+    outputs = {yt.title + ".mp3" : None}
+    )
+
+ff.run()
+
+os.remove(yt.title + ".mp4")
+
+
