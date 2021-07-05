8468589ba9da36d16b2ba3de814f29c10541c24c
Bartlomiej Mikler <bartlomiej.mikler@gmail.com>
2021-07-05 18:45:03 +0200

    bug with special characters in file name repaired

--- yt.py
+++ yt.py
@@ -2,25 +2,53 @@ from pytube import YouTube
 import ffmpy
 import os
 
-while True:
 
-    yt = input("Videos URL: ")
+def main():
+    while True:
 
-    try:
-        yt = YouTube(yt)
-    except:
-        print("Wrong input!")
-        continue
+        yt = input("Videos URL: ")
 
-    print("Working...")
-    stream = yt.streams.first()
+        # check if input is correct YouTube URL
+        try:
+            yt = YouTube(yt)
+        except:
+            print("Wrong input!")
+            continue
 
-    print("Download...")
+        # generate new title to avoid bugs with special characters
+
+        ytitle = yt.title
+        title = ""
+
+        for i in range(len(ytitle)):
+            if ytitle[i] != "|" and ytitle[i] != '"':
+                title += ytitle[i]
+
+        # chose the stream - best quality form audio only
+        print("Working...")
+        stream = yt.streams.filter(only_audio=True).first()
+
+        # check if file exist
+        if os.path.exists(title + ".mp3"):
+            print("File already exist")
+        else:
+            download(stream)
+            convert(title)
+
+
+# download mp4
+def download(stream):
+    print("Downloading...")
     stream.download()
 
+
+# convert mp4 into mp3 and delete mp4 file
+def convert(title):
+
+    # set ffmpeg
     ff = ffmpy.FFmpeg(
-        inputs={yt.title + ".mp4": None},
-        outputs={yt.title + ".mp3": None}
+        inputs={title + ".mp4": None},
+        outputs={title + ".mp3": None}
     )
 
     try:
@@ -30,6 +58,8 @@ while True:
     except:
         os.system("cls")
         print("\nError during conversion into .mp3")
-        continue
     finally:
-        os.remove(yt.title + ".mp4")
+        os.remove(title + ".mp4")
+
+
+main()
